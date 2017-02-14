d431cc3b24c730b81d960ca4f2f2b39e4303e4f3
Dom Chino <blankcreativegroup@gmail.com>
2017-02-14 17:02:54 -0500

    Created users with username and other attributes. minor stylization for  login sign up and edit forms. styled the alert and notice messages.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,8 +1,15 @@
 class PagesController < ApplicationController
+
   def index
+    if user_signed_in?
+      redirect_to(home_path)
+    end
   end
 
   def home
+    if !user_signed_in?
+      redirect_to(new_user_session_path)
+    end
   end
 
   def about
---
+++ db/migrate/20170214205413_add_attributes_to_users.rb
@@ -0,0 +1,9 @@
+class AddAttributesToUsers < ActiveRecord::Migration[5.0]
+  def change
+    add_column :users, :username, :string
+    add_column :users, :artist_name, :string
+    add_column :users, :bio, :text
+
+    add_index :users, :username, unique: true
+  end
+end
